23294f06e9ee396536411873d8c108914cb651c6
Mayo <patrick.mayo.moran@gmail.com>
2021-07-05 10:32:29 -0400

    include all exercises from chapter 1

--- 01_basics/exercises.rb
+++ 01_basics/exercises.rb
@@ -5,17 +5,44 @@
 n = 4167 
 
 # digit in ones place
-
 puts n % 10 
 
 # digit in tens place
-
 puts n / 10 % 10
  
 # digit in hundreds place
-
 puts n / 100 % 10
 
 # digit in thousands place
-
 puts n / 1000
+
+# 3
+my_movies = {
+Lion_King: 1993,
+Gattaca: 2005,
+It_s_a_Wonderful_Life: 1939
+}
+
+puts my_movies[:Lion_King]
+puts my_movies[:Gattaca]
+puts my_movies[:It_s_a_Wonderful_Life]
+
+# 4
+
+my_movie_array = [1993, 2005, 1939]
+puts my_movie_array[0] 
+puts my_movie_array[1] 
+puts my_movie_array[2] 
+
+# 5
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 5 * 4 * 3 * 2 * 1
+
+# 6
+puts 4.2**2
+puts 19.116**2
+puts 0.06**2
+
+

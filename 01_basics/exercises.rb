5e222b9fea12df8f56aad7086882ab9acc4c9fdb
Mayo <patrick.mayo.moran@gmail.com>
2021-07-05 06:36:09 -0400

    include decimal place exercise

---
+++ 01_basics/exercises.rb
@@ -0,0 +1,21 @@
+# 1
+"Patrick " + "Moran"
+
+# 2
+n = 4167 
+
+# digit in ones place
+
+puts n % 10 
+
+# digit in tens place
+
+puts n / 10 % 10
+ 
+# digit in hundreds place
+
+puts n / 100 % 10
+
+# digit in thousands place
+
+puts n / 1000

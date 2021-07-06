2c11f82064f6ba96d3e11956a3e322ab64d73cd5
Mayo <patrick.mayo.moran@gmail.com>
2021-07-06 02:39:46 -0400

    include chapter 6 exercises

---
+++ 06_arrays/check_number.rb
@@ -0,0 +1,8 @@
+arr = [1, 3, 5, 7, 9, 11]
+num = 3
+
+def check(arr, num)
+  arr.include?(num)
+end
+
+puts check(arr, num)
---
+++ 06_arrays/each_with_index.rb
@@ -0,0 +1,4 @@
+a = ["snap", "crackle", "pop"]
+
+a.each_with_index {|name, ind| puts "#{name} is in spot #{ind + 1}"}
+
